f8f531b00c009d338bad437383942623a526b339
William Simas <94724763+williamsimass@users.noreply.github.com>
2025-11-25 15:45:47 -0300

    atualizaçãoapi
    atualizaçãoapi

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://campanhas-backend.onrender.com',
+    baseURL: import.meta.env.VITE_API_URL || 'https://backendcamp.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
